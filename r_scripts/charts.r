d24c60cb411784d6e705dc506f7d7cba7c8fdb87
Marcello Steiner <steiner.marcello@gmail.com>
2015-06-04 11:00:53 +0200

    added charts for average cyclomatic complexity (RQ1)

--- r_scripts/charts.r
+++ r_scripts/charts.r
@@ -5,13 +5,16 @@ avg_uft_jmp4_regression <- read.csv("H:/Desktop/csvs/avg_uft_jmp4_regression.csv
 avg_uft_jmp_smoke <- read.csv("H:/Desktop/csvs/avg_uft_jmp_smoke.csv")
 avg_uft_jmp_birch <- read.csv("H:/Desktop/csvs/avg_uft_jmp_birch.csv")
 avg_uft_smoke <- read.csv("H:/Desktop/csvs/avg_uft_smoke.csv")
+avg_uft_jmp <- read.csv("H:/Desktop/csvs/avg_uft_jmp.csv")
+avg_uft_mr_temp2 <- read.csv("H:/Desktop/csvs/avg_uft_mr_temp2.csv")
 
 #Proness to change datasets
-proness_uft_common <- read.csv("H:/Desktop/csvs/proness_uft_common.csv")
-proness_uft_jmp4_regression <- read.csv("H:/Desktop/csvs/proness_uft_jmp4_regression.csv")
-proness_uft_jmp_smoke <- read.csv("H:/Desktop/csvs/proness_uft_jmp_smoke.csv")
-proness_uft_jmp_birch <- read.csv("H:/Desktop/csvs/proness_uft_jmp_birch.csv")
+#proness_uft_common <- read.csv("H:/Desktop/csvs/proness_uft_common.csv")
+#proness_uft_jmp4_regression <- read.csv("H:/Desktop/csvs/proness_uft_jmp4_regression.csv")
+#proness_uft_jmp_smoke <- read.csv("H:/Desktop/csvs/proness_uft_jmp_smoke.csv")
+#proness_uft_jmp_birch <- read.csv("H:/Desktop/csvs/proness_uft_jmp_birch.csv")
 #proness_uft_smoke <- read.csv("H:/Desktop/csvs/proness_uft_smoke.csv")
+#proness_uft_jmp <- read.csv("H:/Desktop/csvs/proness_uft_jmp.csv")
 
 
 #Needed to stop the script so I can save the image :)
@@ -20,7 +23,7 @@ readkey <- function(){
   line <- readline()
 }
 
-plotLinesForProject <- function(projectName, lineData, lineNames){
+plotLinesForProject <- function(projectName, lineData, lineNames, legendPos="bottomright"){
   plot(
     unlist(lineData),
     type="l",
@@ -31,30 +34,83 @@ plotLinesForProject <- function(projectName, lineData, lineNames){
   )
   mapply(lines,lineData,col=seq_along(lineData),lty=1)
   if(!is.null(lineNames)){
-    legend("bottomright",legend=lineNames,lty=1,col=seq_along(lineData))
+    legend(legendPos,legend=lineNames,lty=1,col=seq_along(lineData))
   }
 }
 
-plotBarsForProject <- function(projectName, barData, barNames){
-  barplot(proness_uft_common$PRONESS, names.arg = proness_uft_common$COMPLEXITY)
-}
+#plotBarsForProject <- function(projectName, barData, barNames){
+#  barplot(proness_uft_common$PRONESS, names.arg = proness_uft_common$COMPLEXITY)
+#}
 
-bar <- list(a=proness_uft_jmp_birch$PRONESS)
-name <- list(a="Test")
-plotBarsForProject("Test", bar, name )
+#bar <- list(a=proness_uft_jmp_birch$PRONESS)
+#name <- list(a="Test")
+#plotBarsForProject("Test", bar, name )
 
 
 
 #Create line chart for Project A
-dat <- list(a=avg_atrium_creweditor$AVG)
-nam <- list(avg="Compound (Smoke + Regression + Performance)")
-plotLinesForProject("Project A", dat, nam)
+data <- list(
+  a=avg_atrium_creweditor$AVG,
+  b=avg_uft_smoke$AVG
+)
+name <- list(
+  a="Repository A1 (Regression tests)",
+  b="Repostiory A2 (Integration + Smoke tests)"
+  )
+plotLinesForProject("Project A", data, name)
+readkey()
+
+#Create line chart for Project B
+data <- list(a=avg_uft_mr_temp2$AVG)
+name <- list(avg="Repository B1 (Smoke tests)")
+plotLinesForProject("Project B", data, name)
+readkey()
+
+#Create line chart for project C
+data <- list(
+  a=avg_uft_jmp$AVG,
+  b=avg_uft_jmp_birch$AVG,
+  c=avg_uft_jmp4_regression$AVG,
+  d=avg_uft_jmp_smoke$AVG
+  )
+name <- list(
+  a="Repository C1 (Regression tests)",
+  b="Repository C2 (Regression tests)",
+  c="Repository C3 (Regression tests)",
+  d="Repository C4 (Smoke tests)"
+  )
+plotLinesForProject("Project C", data, name,"topright")
 readkey()
 
 #Create line chart for Project D
 dat <- list(a=avg_uft_common$AVG)
-nam <- NULL
-plotLinesForProject("Project D", dat, nam)
+nam <- list(a="Repository D1 (Reusable functions)")
+plotLinesForProject("Project D", dat, nam, "topright")
+
+
+#Create line chart Aggregated
+dat <- list(
+  a=avg_atrium_creweditor$AVG,
+  b=avg_uft_smoke$AVG,
+  c=avg_uft_mr_temp2$AVG,
+  d=avg_uft_jmp$AVG,
+  e=avg_uft_jmp_birch$AVG,
+  f=avg_uft_jmp4_regression$AVG,
+  g=avg_uft_jmp_smoke$AVG,
+  h=avg_uft_common$AVG
+  )
+nam <- list(
+  a="A1",
+  b="A2",
+  c="B1",
+  d="C1",
+  e="C2",
+  f="C3",
+  g="C4",
+  h="D1"
+  )
+plotLinesForProject("Overall complexity", dat, nam, "topright")
+
 
 
 
