04644ad85b888ceebbdc0305ee0c14267c07e9a6
Marcello Steiner <steiner.marcello@gmail.com>
2015-06-08 10:42:20 +0200

    Added stacked barplot for rq1 sources

--- r_scripts/charts.r
+++ r_scripts/charts.r
@@ -1,3 +1,5 @@
+library(RColorBrewer)
+
 #Average complexity datasets
 avg_atrium_creweditor <- read.csv("H:/Desktop/csvs/avg_uft_atrium_creweditor.csv")
 avg_uft_common <- read.csv("H:/Desktop/csvs/avg_uft_common.csv")
@@ -15,6 +17,10 @@ avg_uft_mr_temp2 <- read.csv("H:/Desktop/csvs/avg_uft_mr_temp2.csv")
 #proness_uft_jmp_birch <- read.csv("H:/Desktop/csvs/proness_uft_jmp_birch.csv")
 #proness_uft_smoke <- read.csv("H:/Desktop/csvs/proness_uft_smoke.csv")
 #proness_uft_jmp <- read.csv("H:/Desktop/csvs/proness_uft_jmp.csv")
+
+
+
+rq1_sources <- read.csv("C:/Users/marcello.steiner/Desktop/thesis/figure_src/RQ1/RQ1_sources.csv", sep=";")
 
 
 #Needed to stop the script so I can save the image :)
@@ -41,6 +47,22 @@ plotLinesForProject <- function(projectName, lineData, lineNames, legendPos="bot
 #plotBarsForProject <- function(projectName, barData, barNames){
 #  barplot(proness_uft_common$PRONESS, names.arg = proness_uft_common$COMPLEXITY)
 #}
+
+plotBarForSources <- function(dataset, names = NULL, legendPos = "topright"){
+  chartColors <- brewer.pal(length(dataset), "Blues")
+  tmp <- as.matrix(dataset[-1])
+  rownames(tmp) <- dataset[, 1]
+  barplot(
+    t(tmp),
+    col = chartColors
+  )
+  if(!is.null(names)){
+    legend(legendPos,legend=rev(names), fill = rev(chartColors))
+  }
+  
+}
+plotBarForSources(RQ1_sources, names(RQ1_sources)[2:6] )
+
 
 #bar <- list(a=proness_uft_jmp_birch$PRONESS)
 #name <- list(a="Test")
@@ -110,6 +132,10 @@ nam <- list(
   h="D1"
   )
 plotLinesForProject("Overall complexity", dat, nam, "topright")
+
+
+
+
 
 
 
